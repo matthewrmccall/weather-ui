eb24d1f6ce3e3002f280a6a8237c22ff80c02c07
matthewrmccall <matt@mrmccall.com>
2020-12-22 23:25:46 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const api = {
+  key: "b9fc5a67d8e7f7b86c0f6f9b74be3dc7",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search...."
+          />
+        </div>
+      </main>
     </div>
   );
 }
